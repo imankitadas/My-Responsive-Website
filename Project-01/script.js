130efb4a0fe7122f9be680e4ce240c14cb84ba5a
imankitadas <ankitaab07@GMAIL.com>
2024-01-27 17:08:36 +0530

    added

---
+++ Project-01/script.js
@@ -0,0 +1,11 @@
+let nav_menu_icon = document.querySelector(".nav-menu-icon");
+let navlist = document.querySelector(".navlist");
+
+
+nav_menu_icon.addEventListener('click', () => { 
+    
+    console.log('Menu icon clicked');
+    navlist.classList.toggle('v-class');
+    nav_menu_icon.classList.toggle('v-class');
+    
+});
